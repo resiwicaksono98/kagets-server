5a8d9f6e7c508ed9a87884b34213aa68f16742bd
Resi Wicaksono <resiwicaksono98@gmail.com>
2023-01-23 13:02:39 +0700

    production to cylic

--- models/category.js
+++ models/category.js
@@ -1,34 +1,37 @@
-const { v4: uuidv4 } = require('uuid')
-'use strict';
-const {
-  Model
-} = require('sequelize');
+/** @format */
+
+const { v4: uuidv4 } = require("uuid");
+("use strict");
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Category extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      Category.hasMany(models.News, {foreignKey: 'categoryId',as: 'news' })
-    }
-  }
-  Category.init({
-    id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      primaryKey: true,
-      defaultValue: () => uuidv4(),
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-  }, {
-    sequelize,
-    modelName: 'Category',
-  });
-  return Category;
+   class Category extends Model {
+      /**
+       * Helper method for defining associations.
+       * This method is not a part of Sequelize lifecycle.
+       * The `models/index` file will call this method automatically.
+       */
+      static associate(models) {
+         Category.hasMany(models.News, { foreignKey: "categoryId", as: "news" });
+      }
+   }
+   Category.init(
+      {
+         id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            primaryKey: true,
+            defaultValue: () => uuidv4(),
+         },
+         name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+         },
+      },
+      {
+         sequelize,
+         modelName: "Category",
+      }
+   );
+   return Category;
 };
--- models/problem_types.js
+++ models/problem_types.js
@@ -1,32 +1,35 @@
-'use strict';
-const { v4: uuidv4 } = require('uuid')
-const {
-  Model
-} = require('sequelize');
+/** @format */
+
+"use strict";
+const { v4: uuidv4 } = require("uuid");
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Problem_types extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      Problem_types.hasMany(models.Complaint, { foreignKey: 'problem', as: 'complaint' })
-    }
-  }
-  Problem_types.init({
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: () => uuidv4(),
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      primaryKey: true
-    }
-  }, {
-    sequelize,
-    modelName: 'Problem_types',
-  });
-  return Problem_types;
+   class Problem_types extends Model {
+      /**
+       * Helper method for defining associations.
+       * This method is not a part of Sequelize lifecycle.
+       * The `models/index` file will call this method automatically.
+       */
+      static associate(models) {
+         Problem_types.hasMany(models.Complaint, { foreignKey: "problem", as: "complaint" });
+      }
+   }
+   Problem_types.init(
+      {
+         id: {
+            type: DataTypes.UUID,
+            defaultValue: () => uuidv4(),
+         },
+         name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+         },
+      },
+      {
+         sequelize,
+         modelName: "Problem_types",
+      }
+   );
+   return Problem_types;
 };
