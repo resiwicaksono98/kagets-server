5bce8edc79bad9f983d170abbe73dc6b73da62d5
Resi Wicaksono <resiwicaksono98@gmail.com>
2023-01-24 16:59:49 +0700

    setup cloudinary

--- app/news/router.js
+++ app/news/router.js
@@ -5,20 +5,21 @@ const newsController = require("./controller");
 const multer = require("multer");
 const path = require("path");
 const config = require("../config");
+const multerUpload = require("../../config/cloudinary");
 
-const diskStorage = multer.diskStorage({
-   destination: function (req, file, cb) {
-      cb(null, path.resolve(config.rootPath, "public/images/news"));
-   },
-   filename: function (req, file, cb) {
-      cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
-   },
-});
+// const diskStorage = multer.diskStorage({
+//    destination: function (req, file, cb) {
+//       cb(null, path.resolve(config.rootPath, "public/images/news"));
+//    },
+//    filename: function (req, file, cb) {
+//       cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
+//    },
+// });
 
-router.post("/news", multer({ storage: diskStorage }).single("image"), newsController.storeNews);
+router.post("/news", multerUpload.single("image"), newsController.storeNews);
 router.get("/news", newsController.getNews);
 router.get("/news/:id", newsController.getNewsById);
-router.put("/news/:id", multer({ storage: diskStorage }).single("image"), newsController.updateNews);
+// router.put("/news/:id", multer({ storage: diskStorage }).single("image"), newsController.updateNews);
 router.delete("/news/:id", newsController.destroyNews);
 
 module.exports = router;
--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,11 +1,22 @@
 /** @format */
 
-const cloudinary = require("cloudinary");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
 
-const cloudinaryConfig = cloudinary.config({
+cloudinary.config({
    cloud_name: "doh920vsx",
    api_key: "725157489862262",
    api_secret: "0hpZGAP8cDx2rBfc-1LwjmO-AKo",
 });
 
-module.exports = cloudinaryConfig;
+const storage = new CloudinaryStorage({
+   cloudinary: cloudinary,
+   params: {
+      folder: "KAGETS",
+   },
+});
+
+const multerUpload = multer({ storage: storage });
+
+module.exports = multerUpload;
