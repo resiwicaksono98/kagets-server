5ee7d7362c322519f6399e57b36409a690b73c1e
Resi Wicaksono <resiwicaksono98@gmail.com>
2023-01-25 01:24:03 +0700

    complaint multer

--- app/complaint/router.js
+++ app/complaint/router.js
@@ -4,21 +4,22 @@ const router = require("express").Router();
 const complaintController = require("./controller");
 const multer = require("multer");
 const path = require("path");
+const multerUpload = require("../../config/cloudinary");
 
-const diskStorage = multer.diskStorage({
-   destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, "../../public/images/complaints"));
-   },
-   filename: function (req, file, cb) {
-      cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
-   },
-});
+// const diskStorage = multer.diskStorage({
+//    destination: function (req, file, cb) {
+//       cb(null, path.join(__dirname, "../../public/images/complaints"));
+//    },
+//    filename: function (req, file, cb) {
+//       cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
+//    },
+// });
 
 router.get("/complaints", complaintController.getComplaint);
 router.get("/complaints/:id", complaintController.getComplaintById);
 router.get("/complaints/all/:userId", complaintController.getComplaintByUser);
-router.post("/complaints", multer({ storage: diskStorage }).single("support_image"), complaintController.storeComplaint);
-router.put("/complaints/:id", multer({ storage: diskStorage }).single("support_image"), complaintController.updateComplaint);
+router.post("/complaints", multerUpload.single("support_image"), complaintController.storeComplaint);
+router.put("/complaints/:id", multerUpload.single("support_image"), complaintController.updateComplaint);
 router.delete("/complaints/:id", complaintController.destroyComplaint);
 
 module.exports = router;
